e811380b14a8a5365e8960fb2f7d68720d5fa22a
Lara Luís <lr_lara_raquel@hotmail.com>
2021-12-09 00:25:09 +0000

    Update Part1.py

--- Day8/Part1.py
+++ Day8/Part1.py
@@ -1,24 +1,17 @@
 import collections
-import statistics
 
 my_file = open("input", "r")
 content = my_file.read()
 lines = content.split("\n")
 
-for i,line in enumerate(lines):
-    if(i % 2 == 1):
-        digitList = content.split("|")
+count = 0
+for line in lines:
+    digitString = line.strip().split("|")
+    digitListOutput = digitString[1].split(" ")
 
-        digitListOutput = digitList[1].strip().split(" ")
-        count = 0
-
-        for digit in digitListOutput:
-            wordSize = len(digit)
-            if wordSize == 2 or wordSize == 3 or wordSize == 4 or wordSize == 7:
-                count += 1
+    for digit in digitListOutput:
+        wordSize = len(digit)
+        if wordSize == 2 or wordSize == 3 or wordSize == 4 or wordSize == 7:
+            count += 1
 
 print(count)
-
-
-
-
