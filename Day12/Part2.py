ba9cd463eb88be09b71753f49d1844721043ef13
Lara Luís <lr_lara_raquel@hotmail.com>
2021-12-12 19:16:47 +0000

    day 12 part 2

---
+++ Day12/Part2.py
@@ -0,0 +1,70 @@
+my_file = open("input", "r")
+content = my_file.read()
+lines = content.split("\n")
+
+start = list()
+paths = dict()
+
+built_paths = list()
+
+def build_path(currentPlace, currentPath, built_paths, has_visited_twice):
+    if currentPlace.islower():
+        # avoiding to repeat small caves
+        if currentPlace == "start" and currentPath.count(currentPlace) > 0:
+            return
+        if currentPlace == "end" and currentPath.count(currentPlace) > 0:
+            return
+        if currentPath.count(currentPlace) > 0 and has_visited_twice:
+            return
+        elif currentPath.count(currentPlace) > 0:
+            has_visited_twice = True
+    
+    if currentPlace == "end":
+        currentPath.append(currentPlace)
+        built_paths.append(currentPath)
+        return
+    
+    this_place_paths = paths.get(currentPlace)
+    if this_place_paths != None:
+        currentPath.append(currentPlace)
+        for place in this_place_paths:
+            build_path(place, currentPath[:], built_paths, has_visited_twice)
+
+
+for line in lines:
+    line_components = line.split("-")
+    if line_components[0] == "start":
+        start.append(line_components[1])
+
+    if line_components[1] == "start":
+        start.append(line_components[0])
+
+    else:
+        if line_components[0] == "end":
+            tmp = line_components[0]
+            line_components[0] = line_components[1]
+            line_components[0] = tmp
+
+        dests = paths.get(line_components[0])
+        if dests:
+            dests.append(line_components[1])
+        else:
+            new_list = list()
+            new_list.append(line_components[1])
+            paths[line_components[0]] = new_list
+
+        dests = paths.get(line_components[1])
+        if dests:
+            dests.append(line_components[0])
+        else:
+            new_list = list()
+            new_list.append(line_components[0])
+            paths[line_components[1]] = new_list
+
+for path in start:
+    new_path = list()
+    new_path.append("start")
+    build_path(path, new_path[:], built_paths, False)
+
+print(len(built_paths))
+
